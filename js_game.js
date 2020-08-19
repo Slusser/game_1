b5a6e1bb7dd57380c966b82b58cb58e8a29a9126
Slusser <slusarekjakub@gmail.com>
2020-08-19 18:17:43 +0200

    Add files via upload

---
+++ js_game.js
@@ -0,0 +1,13 @@
+var characterName = "Kuba";
+characterName = "Miszcz";
+var numerLives = 0;
+let message = `siema, elo ${characterName}`;
+let length = characterName.length;
+let lowerName = characterName.toLowerCase();
+
+result = characterName == lowerName ? characterName : lowerName;
+console.log(result);
+
+var isOver = !(numerLives >0);
+
+console.log(isOver);
