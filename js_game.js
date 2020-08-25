b2562d140c8c6a3e031395126e5e253da72a266e
Slusser <slusarekjakub@gmail.com>
2020-08-25 19:17:22 +0200

    Add files via upload

--- js_game.js
+++ js_game.js
@@ -1,13 +1,16 @@
-var characterName = "Kuba";
-characterName = "Miszcz";
-var numerLives = 0;
-let message = `siema, elo ${characterName}`;
-let length = characterName.length;
-let lowerName = characterName.toLowerCase();
+function gameChar(name,xPos,health){
+    this.name=name;
+    this.xPos=xPos;
+    this.health=health;
+    this.move = function(x){
+        this.xPos += x;
+    }
+    this.class="Human"
+}
 
-result = characterName == lowerName ? characterName : lowerName;
-console.log(result);
+var gc1 = new gameChar("lol", 4, 120)
+gc1.move(2)
+console.log(gc1.xPos)
 
-var isOver = !(numerLives >0);
-
-console.log(isOver);
+var gc2 = new gameChar("pols", 1, 234)
+console.log(gc2.name+" "+gc2.class)
